7cbfba6f309b3d9d7c2e557c678b6b33040e2691
Helvio Pedreschi <helvio.pedreschi@ifm.com>
2019-01-04 12:35:55 -0500

    Dumb Error

--- src/app/navigator/snipe-sheet.component.ts
+++ src/app/navigator/snipe-sheet.component.ts
@@ -1,18 +1,18 @@
 import { Component, Inject } from '@angular/core';
 import { SnipeService } from '../_services/snipe.service';
-import { MatDialog, MAT_DIALOG_DATA } from '@angular/material';
+import { MatDialog, MAT_BOTTOM_SHEET_DATA } from '@angular/material';
 import { Snipe } from '../_interfaces/snipe';
 import { NewSnipeComponent } from './new-snipe.component';
 
 @Component({
   template: `
   <mat-nav-list>
-    <mat-list-item (click)="edit(data.snipe)">
+    <mat-list-item (click)="edit(snipe)">
       <mat-icon matListIcon>edit</mat-icon>
       <h3 matLine>Edit</h3>
       <p matLine>Edit the Snipe Settings</p>
     </mat-list-item>
-    <mat-list-item (click)="delete(data.snipe)">
+    <mat-list-item (click)="delete(snipe)">
       <mat-icon matListIcon>delete</mat-icon>
       <h3 matLine>Delete</h3>
       <p matLine>Delete the Scheduled Snipe</p>
@@ -25,7 +25,7 @@ export class SnipeSheetComponent  {
   constructor(
     private service: SnipeService,
     private dialog: MatDialog,
-    @Inject(MAT_DIALOG_DATA) public data: any) { }
+    @Inject(MAT_BOTTOM_SHEET_DATA) public snipe: Snipe) { }
 
     edit(snipe: Snipe): void {
       this.dialog.open(NewSnipeComponent, {data: {snipe: snipe, title: 'Edit Snipe'}}).afterClosed().subscribe(() => {});
